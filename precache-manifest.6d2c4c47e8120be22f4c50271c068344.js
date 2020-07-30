ea51f0cdea405c48d1ad58cb7d19e1fb37cadedd
Lashonda Miller <lashondamiller007@gmail.com>
2020-07-30 10:15:17 -0400

    Updates

---
+++ precache-manifest.6d2c4c47e8120be22f4c50271c068344.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb97ba126736a586243e0ebd9e34426",
+    "url": "/second-react-lecture/index.html"
+  },
+  {
+    "revision": "474a547dc5331ef8b72d",
+    "url": "/second-react-lecture/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e481f2b51e895198772e",
+    "url": "/second-react-lecture/static/js/2.551833ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/second-react-lecture/static/js/2.551833ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "474a547dc5331ef8b72d",
+    "url": "/second-react-lecture/static/js/main.c27b12bd.chunk.js"
+  },
+  {
+    "revision": "61f52f86beb30ee46c47",
+    "url": "/second-react-lecture/static/js/runtime-main.ea430201.js"
+  }
+]);
